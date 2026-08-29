531d976246285995fb24bb75975ffc264b5a7823
AsfandyarKhan72 <asfandach72@gmail.com>
2026-08-29 10:25:22 -0600

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pinnstudio",
-    version="0.1.0",
+    version="1.1.0",
     author="AsfandyarKhan72",
     description="No-code GUI for Physics-Informed Neural Networks using DeepXDE",
     packages=find_packages(),
